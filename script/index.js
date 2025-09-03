93505e3e5fa949e3a138b0f5c7ad3f982fce83b6
MehediHasanRafi00 <mehedihasanrafi205@gmail.com>
2025-09-04 02:51:49 +0600

    add highlight active lesson btn functionality

--- script/index.js
+++ script/index.js
@@ -4,11 +4,22 @@ const loadLessons = () => {
     .then((json) => displayLesson(json.data));
 };
 
+const removeActive = () => {
+  const lessonBtn = document.querySelectorAll(".lesson-btn");
+  lessonBtn.forEach((btn) => btn.classList.remove("active"));
+};
+
 const loadLevelWord = (id) => {
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayLevelWord(data.data));
+    .then((data) => {
+      removeActive(); // remove all active class
+      const clickBtn = document.getElementById(`lesson-btn-${id}`);
+
+      clickBtn.classList.add("active"); // add active class
+      displayLevelWord(data.data);
+    });
 };
 
 const displayLevelWord = (words) => {
@@ -60,7 +71,7 @@ const displayLesson = (lessons) => {
 
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-             <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+             <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn">
                 <i class="fa-solid fa-book-open"></i>
                 Lesson - ${lesson.level_no}
               </button>
